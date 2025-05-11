fe8d2e5312003781a72a8acb283ec7866246dbd5
nrhdyt3012 <dayats354313@gmail.com>
2025-05-11 14:26:35 +0700

    add books

---
+++ handler.js
@@ -0,0 +1,72 @@
+const { nanoid } = require('nanoid');
+const books = [];
+
+const addBookHandler = (request, h) => {
+  const { name, year, author, summary, publisher, pageCount, readPage, reading } = request.payload;
+
+  // Validasi: Nama harus ada
+  if (!name) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal menambahkan buku. Mohon isi nama buku',
+    });
+    response.code(400);
+    return response;
+  }
+
+  // Validasi: readPage tidak boleh lebih besar dari pageCount
+  if (readPage > pageCount) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount',
+    });
+    response.code(400);
+    return response;
+  }
+
+  const id = nanoid(16);
+  const finished = pageCount === readPage;
+  const insertedAt = new Date().toISOString();
+  const updatedAt = insertedAt;
+
+  const newBook = {
+    id,
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    finished,
+    reading,
+    insertedAt,
+    updatedAt,
+  };
+
+  books.push(newBook);
+
+  const isSuccess = books.some((book) => book.id === id);
+
+  if (isSuccess) {
+    const response = h.response({
+      status: 'success',
+      message: 'Buku berhasil ditambahkan',
+      data: {
+        bookId: id,
+      },
+    });
+    response.code(201);
+    return response;
+  }
+
+  // Gagal menambahkan (seharusnya jarang terjadi)
+  const response = h.response({
+    status: 'error',
+    message: 'Buku gagal ditambahkan',
+  });
+  response.code(500);
+  return response;
+};
+
+module.exports = { addBookHandler };
--- server.js
+++ server.js
@@ -1,23 +1,19 @@
-const http = require('http');
+const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
 
-/**
- * Logika untuk menangani dan menanggapi request dituliskan pada fungsi ini
- * 
- * @param request: objek yang berisikan informasi terkait permintaan
- * @param response: objek yang digunakan untuk menanggapi permintaan
- */
-const requestListener = (request, response) => {
-    response.setHeader('Content-Type', 'text/html');
- 
-    response.statusCode = 200;
-    response.end('<h1>Halo HTTP Server!</h1>');
 
+const init = async () => {
+  const server = Hapi.server({
+    port: 9000,
+    host: 'localhost',
+    routes: {
+    cors: {
+      origin: ['*'],
+    },
+  },
+  });
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
 };
- 
-const server = http.createServer(requestListener);
-const port = 5000;
-const host = 'localhost';
- 
-server.listen(port, host, () => {
-    console.log(`Server berjalan pada http://${host}:${port}`);
-});
+
+init();
