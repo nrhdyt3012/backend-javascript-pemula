00f4c365d919ab4a2ff2ed34f7224dfbec18eefb
nrhdyt3012 <dayats354313@gmail.com>
2025-05-11 14:28:58 +0700

    eslint fix 1

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
 const Hapi = require('@hapi/hapi');
-const routes = require('./routes');
-
 
 const init = async () => {
   const server = Hapi.server({
     port: 9000,
     host: 'localhost',
     routes: {
-    cors: {
-      origin: ['*'],
+      cors: {
+        origin: ['*'],
+      },
     },
-  },
   });
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
